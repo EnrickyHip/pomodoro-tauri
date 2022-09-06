98ae043cad32150ca129808c103e7592fd058611
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-09-06 13:57:36 -0300

    set timer hour cycle to h23

--- src/utils/convertToTime.ts
+++ src/utils/convertToTime.ts
@@ -1,4 +1,4 @@
 export function convertToTime(miliseconds: number) {
   const time = new Date(miliseconds * 1000);
-  return time.toLocaleTimeString(navigator.language, { timeZone: 'utc' });
+  return time.toLocaleTimeString(navigator.language, { timeZone: 'utc', hourCycle: 'h23' });
 }
