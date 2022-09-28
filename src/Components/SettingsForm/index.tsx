8161c960b673a3d3bdd4058dcef89a2ce86d799a
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-09-27 21:22:57 -0300

    fix empty settings

--- src/Components/SettingsForm/index.tsx
+++ src/Components/SettingsForm/index.tsx
@@ -13,8 +13,15 @@ export function SettingsForm() {
     writeTextFile('settings.json', JSON.stringify(settings), { dir: BaseDirectory.App });
   }, [settings]);
 
-  function handleChange(value: string, max: number, action: (payload: number) => void) {
-    let valueNumber = parseInt(value);
+  function handleChange(value: string, max: number, action: (payload: number) => void, blur = false) {
+    let valueNumber: number;
+
+    if (blur && !value) {
+      valueNumber = 1;
+    } else {
+      valueNumber = parseInt(value);
+    }
+
     if (valueNumber < 1) valueNumber = 1;
     if (valueNumber > max) valueNumber = max;
     action(valueNumber);
@@ -25,7 +32,8 @@ export function SettingsForm() {
       <InputLabel
         label="Work Time (in minutes):"
         id="main-time"
-        onChange={(event) => handleChange(event.target.value, 1439, changeDefaultTime)}
+        onBlur={({ target }) => handleChange(target.value, 1439, changeDefaultTime, true)}
+        onChange={({ target }) => handleChange(target.value, 1439, changeDefaultTime)}
         value={settings.defaultMainTime.toString()}
         type="number"
         min="1"
@@ -33,7 +41,8 @@ export function SettingsForm() {
 
       <InputLabel
         label="Short Rest Time:"
-        onChange={(event) => handleChange(event.target.value, 1439, changeShortTime)}
+        onBlur={({ target }) => handleChange(target.value, 1439, changeShortTime, true)}
+        onChange={({ target }) => handleChange(target.value, 1439, changeShortTime)}
         id="short-rest-time"
         value={settings.shortRestTime.toString()}
         type="number"
@@ -42,7 +51,8 @@ export function SettingsForm() {
 
       <InputLabel
         label="Long Rest Time:"
-        onChange={(event) => handleChange(event.target.value, 1439, changeLongTime)}
+        onBlur={({ target }) => handleChange(target.value, 1439, changeLongTime, true)}
+        onChange={({ target }) => handleChange(target.value, 1439, changeLongTime)}
         id="long-rest-time"
         value={settings.longRestTime.toString()}
         type="number"
@@ -51,7 +61,8 @@ export function SettingsForm() {
 
       <InputLabel
         label="Cycles:"
-        onChange={(event) => handleChange(event.target.value, 100, ChangeCycles)}
+        onBlur={({ target }) => handleChange(target.value, 1439, ChangeCycles, true)}
+        onChange={({ target }) => handleChange(target.value, 100, ChangeCycles)}
         id="cycles"
         value={settings.cycles.toString()}
         type="number"
--- src/Components/UI/InputLabel/index.tsx
+++ src/Components/UI/InputLabel/index.tsx
@@ -6,17 +6,18 @@ import { Container } from './styled';
 interface Props {
   id: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: ChangeEvent<HTMLInputElement>) => void;
   value: string;
-  type: string;
-  min: string;
+  type?: string;
+  min?: string;
   label: string;
 }
 
-export function InputLabel({ id, value, min, onChange, type, label }: Props) {
+export function InputLabel({ id, value, min, onChange, onBlur, type = 'text', label }: Props) {
   return (
     <Container>
       <Label htmlFor={id}>{label}</Label>
-      <Input onChange={onChange} id={id} value={value} type={type} min={min} />
+      <Input onBlur={onBlur} onChange={onChange} id={id} value={value} type={type} min={min} />
     </Container>
   );
 }
